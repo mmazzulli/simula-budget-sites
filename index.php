6eb55c0a03054d919e95fdfe01e028455f58c63e
mmazzulli <webmarchesin@gmail.com>
2022-06-14 16:47:19 +0100

    Inserção dos primeiros files

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+// require __DIR__ . '/app/core/config.php';
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>ORÇAMENTO DE SITES</title>
+    <link rel="stylesheet" href="./resources/css/style.css">
+    <link rel="stylesheet" 
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+</head>
+<body>
+
+    
+</body>
+</html>
+
+
+
+
